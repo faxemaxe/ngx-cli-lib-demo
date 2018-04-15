0bbc31d3b86812053d1e4f050d51f4780aba8f63
Max Höffner <maxhoeffner@aol.com>
2018-04-15 17:41:44 +0200

    provide proper return types to satisfy AOT build

--- projects/ngx-cli-lib-demo-crud/src/lib/ngx-cli-lib-demo-crud.service.ts
+++ projects/ngx-cli-lib-demo-crud/src/lib/ngx-cli-lib-demo-crud.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,7 @@ export abstract class NgxCliLibDemoCrudService {
     private httpClient: HttpClient,
   ) { }
 
-  public post(data: any) {
+  public post(data: any): Observable<any> {
     return this.httpClient.post(
       `${this.baseUrl}/${this.endPoint}`,
       data
@@ -28,7 +29,7 @@ export abstract class NgxCliLibDemoCrudService {
     );
   }
 
-  public get(id: number) {
+  public get(id: number): Observable<any>  {
     return this.httpClient.get(
       `${this.baseUrl}/${this.endPoint}/${id}`,
     ).pipe(
@@ -40,7 +41,7 @@ export abstract class NgxCliLibDemoCrudService {
     );
   }
 
-  public put(data: any) {
+  public put(data: any): Observable<any> {
     return this.httpClient.put(
       `${this.baseUrl}/${this.endPoint}`,
       data
@@ -53,7 +54,7 @@ export abstract class NgxCliLibDemoCrudService {
     );
   }
 
-  public delete(id: number) {
+  public delete(id: number): Observable<any> {
     return this.httpClient.delete(
       `${this.baseUrl}/${this.endPoint}/${id}`,
     ).pipe(
