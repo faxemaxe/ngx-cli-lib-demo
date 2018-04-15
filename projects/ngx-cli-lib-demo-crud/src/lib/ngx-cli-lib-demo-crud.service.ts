1a4914a3f582bd0d4c717d834c805341852ddebc
Max Höffner <maxhoeffner@aol.com>
2018-04-15 15:48:48 +0200

    add proper service logic for out lib

--- projects/ngx-cli-lib-demo-crud/src/lib/ngx-cli-lib-demo-crud.service.ts
+++ projects/ngx-cli-lib-demo-crud/src/lib/ngx-cli-lib-demo-crud.service.ts
@@ -1,9 +1,68 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class NgxCliLibDemoCrudService {
+export abstract class NgxCliLibDemoCrudService {
 
-  constructor() { }
+  protected abstract baseUrl: string;
+  protected abstract endPoint: string;
+  protected abstract parseData: any;
+
+  constructor(
+    private httpClient: HttpClient,
+  ) { }
+
+  public post(data: any) {
+    return this.httpClient.post(
+      `${this.baseUrl}/${this.endPoint}`,
+      data
+    ).pipe(
+      map(
+        (resp) => {
+          return this.parseData(resp);
+        }
+      )
+    );
+  }
+
+  public get(id: number) {
+    return this.httpClient.get(
+      `${this.baseUrl}/${this.endPoint}/${id}`,
+    ).pipe(
+      map(
+        (resp) => {
+          return this.parseData(resp);
+        }
+      )
+    );
+  }
+
+  public put(data: any) {
+    return this.httpClient.put(
+      `${this.baseUrl}/${this.endPoint}`,
+      data
+    ).pipe(
+      map(
+        (resp) => {
+          return this.parseData(resp);
+        }
+      )
+    );
+  }
+
+  public delete(id: number) {
+    return this.httpClient.delete(
+      `${this.baseUrl}/${this.endPoint}/${id}`,
+    ).pipe(
+      map(
+        (resp) => {
+          return this.parseData(resp);
+        }
+      )
+    );
+  }
+
 }
