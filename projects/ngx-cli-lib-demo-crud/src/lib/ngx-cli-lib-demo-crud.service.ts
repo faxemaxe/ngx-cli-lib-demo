25075ba1f17debe6284d2d82434a349ca30be66f
Max Höffner <maxhoeffner@aol.com>
2018-07-29 10:25:44 +0200

    remove Injectable decorator from abstract class since it is not needed

--- projects/ngx-cli-lib-demo-crud/src/lib/ngx-cli-lib-demo-crud.service.ts
+++ projects/ngx-cli-lib-demo-crud/src/lib/ngx-cli-lib-demo-crud.service.ts
@@ -1,11 +1,7 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
 export abstract class NgxCliLibDemoCrudService {
 
   protected abstract baseUrl: string;
